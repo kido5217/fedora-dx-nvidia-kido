55299162994eae544223eb2e91cc7bc41d082bfc
Fedor Suchkov <f.suchkov@gmail.com>
2026-05-30 13:21:03 +0300

    Format sh files

--- build_files/10-zfs.sh
+++ build_files/10-zfs.sh
@@ -4,7 +4,7 @@ set -ouex pipefail
 
 ## Add ZFS repo
 
-dnf -y install  https://zfsonlinux.org/fedora/zfs-release-3-1.fc44.noarch.rpm
+dnf -y install https://zfsonlinux.org/fedora/zfs-release-3-1.fc44.noarch.rpm
 
 ## Install kernel headers
 
@@ -16,8 +16,8 @@ dnf -y install zfs
 
 ## Always load zfs moudle
 
-echo 'zfs' > /etc/modules-load.d/zfs.conf
-echo 'zfs' > /etc/dnf/protected.d/zfs.conf
+echo 'zfs' >/etc/modules-load.d/zfs.conf
+echo 'zfs' >/etc/dnf/protected.d/zfs.conf
 
 ## Cleanup
 
